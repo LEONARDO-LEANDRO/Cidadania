28d2856221e9be380b003cc8b78851655a94e031
LEONARDO SOUZA LEANDRO <128157527+LEONARDO-LEANDRO@users.noreply.github.com>
2024-03-11 09:06:42 -0300

    Add files via upload

---
+++ leo.js
@@ -0,0 +1,16 @@
+// Variáveis representando idade e cidadania
+let idade = 21;
+let cidadania = false;
+
+// Verificar se a idade é maior
+
+if (idade >= 18 && cidadania == true)
+    {console.log ('Pode votar!');
+}
+else if (idade<18 && cidadania == true){
+        console.log('Não pode votar!');
+        
+}else if (idade >= 18 && cidadania == false){
+        console.log('Você não pode votar!')
+    
+}
